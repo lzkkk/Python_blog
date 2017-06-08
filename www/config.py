4d4ad102d60e7c195a17180a076013e79857d5f0
wangliping <wangliping@pingan.com.cn>
2017-06-08 20:57:29 +0800

    添加博客、注册、登录等页面

--- www/config.py
+++ www/config.py
@@ -5,17 +5,16 @@
 Configuration
 '''
 
-__author__ = 'lip.W'x
+__author__ = 'lip.W'
 
 import config_default
 
 class Dict(dict):
-	'''
-	Simple dict but support x.y style
-	'''
-
-	def __init__(self, names=(), values=(), **kw):
-		super(Dict, self).__init__(**kw)
+    '''
+    Simple dict but support access as x.y style.
+    '''
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
             self[k] = v
 
@@ -28,28 +27,25 @@ class Dict(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
-
 def merge(defaults, override):
-	r = {}
-	for k,v in defaults.items():
-		if k in override:
-			if isinstance(v, dict):
-				merge(v, override[k])
-			else:
-				r[k] = override[k]
-		else:
-			r[k] = v
-
-	return r
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
 
 def toDict(d):
-	D = Dict()
-	for k, v in d.items():
-		D[k] = toDict(v) if isinstance(v, dict) else v
-	return D
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
 
-
-config = config_default.configs
+configs = config_default.configs
 
 try:
     import config_override
